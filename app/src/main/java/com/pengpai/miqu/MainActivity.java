77bbfcb7d11a2494384ad4133fe6910d846947c7
Mr.XieXie <1438875934@qq.com>
2018-12-29 11:38:20 +0800

    Add gradle-wrapper.

--- app/src/main/java/com/pengpai/miqu/MainActivity.java
+++ app/src/main/java/com/pengpai/miqu/MainActivity.java
@@ -23,6 +23,7 @@ public class MainActivity extends AppCompatActivity{
         Context context = getApplicationContext();
 
         printLog(context);
+
     }
 
     private void printLog(Context context) {
